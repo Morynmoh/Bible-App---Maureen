13b2cd6328fe8c3052210ad25be04536ea9c6f54
Maureen <morynmoh@gmail.com>
2023-04-25 19:33:48 +0300

    bible_context db.json

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Bible-App---Maureen
-Bible Application
-This application takes the data from a public Bible API to poulate the requsted Bible verses from different versions.
+Project Name: Bible Application
+Project Author: Maureen Omamo
+Project Setup Instructions:
+Link to Github pages: 
+Project Description: This application takes the data from a public Bible API to poulate the requsted Bible verses from different versions.
 
 The requested verse is then styled with appropriate backgrounds, colours and can be shared.
+
+API URL: https://bible-api.com | bible_context.json
+
+
+
+
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+const booksBar = document.getElementById("context-bar");
+fetch("http://localhost:3000/bible")
+    .then(response => response.json())
+    .then(books => {
+        books.forEach(book_name => {
+            const bookDiv = document.createElement("div");
+            //give the div a class name 
+            bookDiv.className = "books";
+            
+            bookDiv.innerText = book_name.book;
+            booksBar.appendChild(bookDiv);
+            // on click on the any book , it will show the chapters of that book 
+            bookDiv.addEventListener("click", () => {
 
+                
+                 console.log(book_name.chapters.length);
+                 //create a div for each chapters by looping the chapter length each div class name is chapter
+                    for (let i = 0; i < book_name.chapters.length; i++) {
+                        const chapterDiv = document.createElement("div");
+                        chapterDiv.className = "chapter";
+                        chapterDiv.innerText = i+1;
+                        bookDiv.appendChild(chapterDiv);
+                        //on click on the chapter show show bookname[i].verses
+                        chapterDiv.addEventListener("click", () => {
+                            console.log(book_name.chapters[i].verses);
+                            //create a div for each verses by looping the verses length each div class name is verse
+                            for (let j = 0; j < book_name.chapters[i].verses; j++) {
+                                const verseDiv = document.createElement("div");
+                                verseDiv.className = "verse";
+                                verseDiv.innerText = j+1;
+                                chapterDiv.appendChild(verseDiv);
+                            }
+                        }, {once: true}); // add the {once: true} option here
+                    };
+
+                bookDiv.removeEventListener("click", () => {
+                    console.log(book_name.abbr);
+                });
+
+            }, {once: true}); // add the {once: true} option here too
+        });
+
+        //add click for button stoggle show and hide of the books div the button class is books_toggle
+
+    });
+    // add click event listerner to the button to add inline style options_books, show and hide
+
+
+
+
+ //
